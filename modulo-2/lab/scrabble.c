262a37d7986be66079ed676db40599885f421fb0
SheilaNS <shei.nsantos@gmail.com>
2023-01-12 11:43:42 -0300

    feat: Module-2 lab 02 - scrabble.c

---
+++ modulo-2/lab/scrabble.c
@@ -0,0 +1,52 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+char ALPHA[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // TODO: Print the winner
+    if (score1 == score2)
+    {
+        printf("Tie!\n");
+    }
+    else if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else
+    {
+        printf("Player 2 wins!");
+    }
+}
+
+int compute_score(string word)
+{
+    // TODO: Compute and return score for string
+    int points = 0;
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        for (int l = 0; l < 26; l++)
+        {
+            if (tolower(word[i]) == ALPHA[l])
+            {
+                points += POINTS[l];
+            }
+        }
+    }
+    return points;
+}
